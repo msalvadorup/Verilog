1162a460370945bdb683c6b32e452f81a1d22486
Rusty <rusty.mina@eee.upd.edu.ph>
2018-03-17 18:11:50 +0800

    Separated ML module (top) from the memory

--- top-with-enable/tb_top.v
+++ top-with-enable/tb_top.v
@@ -1,7 +1,8 @@
 `timescale 1ns/1ps
 `define WORD_WIDTH 16
 
-//`include "top.v"
+`include "top.v"
+`include "memory.v"
 
 module tb_top();	
 	reg clock, nrst, en;
@@ -9,8 +10,14 @@ module tb_top();
 	wire done_reward;
 	wire [`WORD_WIDTH-1:0] reward_out;
 
+	// Memory Module Instantiation
+	wire [`WORD_WIDTH-1:0] mem_data_out, mem_data_in;
+	wire [10:0] address;
+	wire wr_en;
+	mem mem1(clock, address, wr_en, mem_data_in, mem_data_out);	
+
 	// Top Module Instantiation
-	top t1(clock, nrst, en, fsourceID, fbatteryStat, fValue, fclusterID, fdestinationID, reward_out, done_reward);
+	top t1(clock, nrst, en, address, wr_en, mem_data_in, mem_data_out, fsourceID, fbatteryStat, fValue, fclusterID, fdestinationID, reward_out, done_reward);
 
 	// Clock
 	initial begin
@@ -47,10 +54,10 @@ module tb_top();
 	end
 
 	initial begin
-		//$dumpfile("tb_top.vcd");
-		//$dumpvars(0, tb_top);
-        	$vcdplusfile("tb_top.vpd"); //$dumpfile("tb_randomGenerator.vcd");
-        	$vcdpluson;
+		$dumpfile("tb_top.vcd");
+		$dumpvars(0, tb_top);
+        //$vcdplusfile("tb_top.vpd"); //$dumpfile("tb_randomGenerator.vcd");
+        //$vcdpluson;
 
 		#10000
 		$finish;
--- top-with-enable/top.v
+++ top-with-enable/top.v
@@ -34,8 +34,6 @@
 				2. Ilagay sa registers at initialize sa nrst or enable
  */
 
-/*
-`include "memory.v"
 `include "mux_11bit.v"
 `include "mux_16bit.v"
 `include "mux_1bit.v"
@@ -51,17 +49,18 @@
 `include "rngAddress.v"
 `include "selectMyAction.v"
 `include "reward.v"
-*/
 
-module top(clock, nrst, en, fsourceID, fbatteryStat, fValue, fclusterID, fdestinationID, reward_out, done_reward);
+
+module top(clock, nrst, en, address, wr_en, mem_data_in, mem_data_out, fsourceID, fbatteryStat, fValue, fclusterID, fdestinationID, reward_out, done_reward);
 	input clock, nrst, en;
 	input [`WORD_WIDTH-1:0] fsourceID, fbatteryStat, fValue, fclusterID, fdestinationID;
-	wire wr_en;
-	// MEMORY MODULE
-	wire [`WORD_WIDTH-1:0] mem_data_in, mem_data_out; 
-	wire [10:0] address;
-	mem mem1(clock, address, wr_en, mem_data_in, mem_data_out);
-	
+
+	// MEMORY MODULE CONNECTIONS
+	input [`WORD_WIDTH-1:0] mem_data_out;
+	output [10:0] address;
+	output [`WORD_WIDTH-1:0] mem_data_in;
+	output wr_en;
+
 	// MUX MODULE
 	reg [2:0] addr_select, wr_select;
 	
